2383e3ab9a761c673b6b156ff5761efbaafba023
azharluthfi14 <35468624+azharluthfi14@users.noreply.github.com>
2021-10-31 02:40:38 +0700

    create configuration for API documentation.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
-from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
